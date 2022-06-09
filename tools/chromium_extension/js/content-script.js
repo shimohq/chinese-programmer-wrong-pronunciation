c73538a01b19683366fa898c5c60fc4cc3f53413
jingjingxyk <zonghengbaihe521@qq.com>
2022-06-09 14:35:48 +0800

    新增README.md页面无跳转即可收听正确读音 的 chromium 扩展

---
+++ tools/chromium_extension/js/content-script.js
@@ -0,0 +1,31 @@
+{
+
+    let URLObj = new URL(location.href)
+    console.log(URLObj)
+
+    let audio_player = new Audio()
+    audio_player.setAttribute('autoplay', 'true')
+    document.querySelector("#readme table tbody").addEventListener('click', (event) => {
+        console.log(event)
+        console.log(event.target.nodeType)
+        console.log(event.target.nodeName)
+        event.preventDefault();
+        event.stopPropagation();
+
+        let audio_url = null;
+        if (event.target.nodeName === 'TD') {
+            let aTag = event.target.querySelector('a');
+            if (aTag) {
+                audio_url = aTag.getAttribute('href');
+            }
+        }
+        if (event.target.nodeName === 'IMG') {
+            let aTag = event.target.parentNode.parentNode
+            audio_url = aTag.getAttribute('href');
+        }
+        if (audio_url) {
+            console.log("audio_url:", audio_url)
+            audio_player.setAttribute('src', audio_url);
+        }
+    })
+}
