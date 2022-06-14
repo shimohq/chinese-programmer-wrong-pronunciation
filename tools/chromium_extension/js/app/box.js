3a9954fa79cab7f28dde01205e714c93415df43a
jingjingxyk <zonghengbaihe521@qq.com>
2022-06-15 04:37:07 +0800

    新增搜索引擎读音检索功能

---
+++ tools/chromium_extension/js/app/box.js
@@ -0,0 +1,39 @@
+import * as until from "./until.js";
+import * as components from "./components.js"
+import * as searchEngine from "./search-engine.js"
+
+let goToSearchPronounce = (word) => {
+
+    let search_engine_name = "gotToGoogleSearch"
+    // search_engine_name='gotToYouDaoSearch'
+    let tab = "current_tab"
+
+    let opener = components.getSearchEngineOpener();
+    if (opener && opener.expired_date) {
+        if(opener.expired_date > (new Date()).getTime()) {
+            if (opener.search_engine_name) {
+                search_engine_name = opener.search_engine_name;
+            }
+            if (opener.tab) {
+                tab = opener.tab;
+            }
+        }
+    }
+    if (search_engine_name && searchEngine[search_engine_name]) {
+        console.log(word)
+        let url = searchEngine[search_engine_name](word);
+        console.log(url)
+        if (tab === 'new_tab') {
+            window.open(url, '_blank')
+        } else {
+            let iframe = components.getIframe();
+            iframe.setAttribute('src', url);
+        }
+    } else {
+        console.log('search engine no found !')
+    }
+
+}
+
+let box = {...until, ...components,...searchEngine, goToSearchPronounce};
+export default box
---
+++ tools/chromium_extension/js/app/until.js
@@ -0,0 +1,24 @@
+function hasClass(el, className) {
+    if (el.classList)
+        return el.classList.contains(className);
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
+
+
+function addClass(el, className) {
+    if (el.classList)
+        el.classList.add(className)
+    else if (!hasClass(el, className))
+        el.className += " " + className;
+}
+
+function removeClass(el, className) {
+    if (el.classList)
+        el.classList.remove(className)
+    else if (hasClass(el, className)) {
+        let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+        el.className = el.className.replace(reg, ' ');
+    }
+}
+
+export {addClass, removeClass}
